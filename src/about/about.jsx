e87f9b1cec8437bfe20b4501190542cef45561a9
RoughRoman <likewise52@gmail.com>
2023-08-17 12:58:58 +0200

    Added conditional rendering for mobile devices on the about section.

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,9 +2,15 @@
 import facePic from '../assets/images/facepic.jpg';
 import './about.css'
 
+// mobile rendering detection package
+import {isMobile} from 'react-device-detect';
+
 export default function AboutSection(){
-    return(
-        <div className="aboutSection">
+
+    if(isMobile){
+
+        return(
+            <div className="aboutSection">
             <h2>About</h2>
             <div className="aboutBody">
                 <p> I am a dedicated and passionate computer scientist, with a strong foundation in
@@ -14,11 +20,35 @@ export default function AboutSection(){
                     Collaborative and effective in communication, I thrive in diverse teams,
                     bringing a valuable contribution to group dynamics. I am fuelled by curiosity and a
                     constant desire to improve, exploring emerging technologies and pushing the
-                    boundaries of technological advancements.</p>
-                <img className='Face' src={facePic} alt=""  />
+                    boundaries of technological advancements.
+                </p>
             </div>
             
         </div>
+        )
+    }
+    else{
+        return(
+            <div className="aboutSection">
+                <h2>About</h2>
+                <div className="aboutBody">
+                    <p> I am a dedicated and passionate computer scientist, with a strong foundation in
+                        problem-solving and a drive for innovative solutions. With a keen eye for detail and a
+                        natural analytical mindset, I excel in tackling complex challenges. I embrace
+                        continuous learning to ensure my skills remain relevant and adaptable.
+                        Collaborative and effective in communication, I thrive in diverse teams,
+                        bringing a valuable contribution to group dynamics. I am fuelled by curiosity and a
+                        constant desire to improve, exploring emerging technologies and pushing the
+                        boundaries of technological advancements.
+                    </p>
+                    <img className='Face' src={facePic} alt="Face"  />
+                </div>
+                
+            </div>
+    
+        )
+    }
 
-    )
+
+    
 }
