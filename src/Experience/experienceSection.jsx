30a190c9931f6dccacc93f30ac599d8999ab0a86
RoughRoman <likewise52@gmail.com>
2023-08-18 19:46:04 +0200

    Added props to experience items within experience section for future scalability

--- src/Experience/experienceSection.jsx
+++ src/Experience/experienceSection.jsx
@@ -7,19 +7,25 @@ export default function ExperienceSection(){
             <h2>Experience</h2>
             <div>
                 
+                <ExperienceItem 
+                    ExperienceProp ={{
+                    title: "University of South Africa",
+                    date: "2019-2022",
+                    body: "This is a test piece of text for the body of an experience."}}
+                />
 
             </div>
         </>
-    )
+    );
 }
 
-function ExperienceItem(titleString, dateString, bodyString){
+function ExperienceItem({ExperienceProp}){
     return(
         <>
         <div className="ExperienceItem">
-            <h3 className="ExperienceTitle">{titleString}</h3>
-            <h4 className="ExperienceDateRange">2019 - 2022</h4>
-            <p className="ExperienceBody">Bachelors of science in computing. </p>
+            <h3 className="ExperienceTitle">{ExperienceProp.title}</h3>
+            <h4 className="ExperienceDateRange">{ExperienceProp.date}</h4>
+            <p className="ExperienceBody">{ExperienceProp.body}</p>
         </div>
         </>
     )
