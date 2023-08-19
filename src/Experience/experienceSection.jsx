d306a7f73f001218cdefa14345235f9e9ee32915
RoughRoman <likewise52@gmail.com>
2023-08-19 14:19:27 +0200

    Added 2 new fields to experience section. Skills and additonal Information. Also did some styling to them.

--- src/Experience/experienceSection.jsx
+++ src/Experience/experienceSection.jsx
@@ -11,12 +11,27 @@ export default function ExperienceSection(){
                     ExperienceProp ={{
                     title: "University of South Africa",
                     date: "2019-2022",
-                    body: "This is a test piece of text for the body of an experience."}}
+                    body: "Degree focused on the core principles of computing "
+                    +     "ranging from algorithms/datastructures, databases, "
+                    +     "and software engineering to formal logic, artificial "
+                    +     "intelligence, and theoretical computer science.",
+                    skills: ["C++","Python", "Qt","NumPy", "Oracle 12c"],
+                    additonalInfo: "Final Average: 75%"}}
                 />
 
             </div>
         </>
     );
+}
+
+function SkillPill({label}){
+    return(
+        <>
+            <div className="SkillPill">
+                <p className="SkillLabel">{label}</p>
+            </div>
+        </>
+    )
 }
 
 function ExperienceItem({ExperienceProp}){
@@ -26,6 +41,12 @@ function ExperienceItem({ExperienceProp}){
             <h3 className="ExperienceTitle">{ExperienceProp.title}</h3>
             <h4 className="ExperienceDateRange">{ExperienceProp.date}</h4>
             <p className="ExperienceBody">{ExperienceProp.body}</p>
+            <div className="SkillPillContainer">
+                {ExperienceProp.skills.map((label) =>
+                    <SkillPill label= {label}/>
+                )}
+            </div>
+            
         </div>
         </>
     )
