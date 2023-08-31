48e26fc90cc84922fd7eee2bfddd8c1641485615
RoughRoman <likewise52@gmail.com>
2023-08-31 18:47:06 +0200

    Added in-development landing page

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+
+let underConstruction = true;
+
+
+if (underConstruction){
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <>
+        <div className='LandingPage'>
+          <h1>Website under Construction</h1>
+          <p>Stay tuned!</p>
+          
+        </div>
+      </>
+    </React.StrictMode>,
+  )
+
+}
+else{
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
