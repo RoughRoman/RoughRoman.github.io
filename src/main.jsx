b9c380b99d648131aa7d30064f84469f90e34f50
RoughRoman <likewise52@gmail.com>
2023-10-24 20:29:39 +0200

    final Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {isMobile} from 'react-device-detect';
+import {isDesktop} from 'react-device-detect';
 
 
 if (isDesktop){
